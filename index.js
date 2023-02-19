58d4c9bfde97bba71a213f6fabfaa11d1f423dc2
Muqads-Riaz <riazahmad03017@gmail.com>
2023-02-19 15:55:40 +0500

    8th Commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const signupUsers = require("./models/signup")
 const todoModel = require("./models/todo")
 const cardModel = require("./models/card")
 const wishCardModel = require("./models/wishCards")
-const requesterModel = require("./models/requester")
+// const requesterModel = require("./models/requester")
 var bcrypt = require('bcryptjs');
 mongoose.connect(dbURi)
   .then((res) => {
@@ -23,7 +23,7 @@ mongoose.connect(dbURi)
 
 
 
-                           //Dummy
+//Dummy
 //Create and Send Data To mongodb 
 // app.post("/user" , (req , res)=>{
 //   const obj = req.body
@@ -118,7 +118,7 @@ mongoose.connect(dbURi)
 
 
 
-                       //Signup & Login
+//Signup & Login
 
 //Signup User
 app.post("/signup", (req, res) => {
@@ -192,7 +192,7 @@ app.post("/login", (req, res) => {
         if (comparePassword) {
           res.json({
             message: "User Login Successfully",
-            data : user ,
+            data: user,
             status: "true"
           })
         } else {
@@ -212,291 +212,306 @@ app.post("/login", (req, res) => {
 
 
 
-                             //Todo
-//Create Todo
-app.post("/todos", (req, res) => {
-  const { todo } = req.body
-  if (!todo) {
-    res.json({
-      message: "Required fields are missing",
-      status: "false"
-    })
-  } else {
-    const objToSend = {
-      todo: todo
-    }
-    todoModel.create(objToSend, (error, data) => {
-      if (error) {
-        res.json({
-          message: "Internal Error",
-          status: "false"
-        })
-      } else {
-        res.json({
-          message: "Todo Created Successfully",
-          data: data,
-          status: "false"
-        })
-      }
-    })
-  }
-})
+//Todo
+// //Create Todo
+// app.post("/todos", (req, res) => {
+//   const { todo } = req.body
+//   if (!todo) {
+//     res.json({
+//       message: "Required fields are missing",
+//       status: "false"
+//     })
+//   } else {
+//     const objToSend = {
+//       todo: todo
+//     }
+//     todoModel.create(objToSend, (error, data) => {
+//       if (error) {
+//         res.json({
+//           message: "Internal Error",
+//           status: "false"
+//         })
+//       } else {
+//         res.json({
+//           message: "Todo Created Successfully",
+//           data: data,
+//           status: "false"
+//         })
+//       }
+//     })
+//   }
+// })
 
-//Send Todos Collection 
-app.get("/todos", (req, res) => {
-  todoModel.find({}, (error, data) => {
-    if (error) {
-      res.json({
-        message: "Internal Error",
-        status: "false"
-      })
-    } else {
-      res.json({
-        message: "Data Get Successfully",
-        data: data,
-        status: "true"
-      })
-    }
-  })
-})
+// //Send Todos Collection 
+// app.get("/todos", (req, res) => {
+//   todoModel.find({}, (error, data) => {
+//     if (error) {
+//       res.json({
+//         message: "Internal Error",
+//         status: "false"
+//       })
+//     } else {
+//       res.json({
+//         message: "Data Get Successfully",
+//         data: data,
+//         status: "true"
+//       })
+//     }
+//   })
+// })
 
-//Delete Todo 
-app.delete("/todos/:id", (req, res) => {
-  const { id } = req.params
-  todoModel.findByIdAndDelete(id, (error, data) => {
-    if (error) {
-      res.json({
-        message: "Internal Error",
-        status: "false"
-      })
-    } else {
-      res.json({
-        message: "Data Deleted Successfully",
-        status: "true"
-      })
-    }
-  })
-})
+// //Delete Todo 
+// app.delete("/todos/:id", (req, res) => {
+//   const { id } = req.params
+//   todoModel.findByIdAndDelete(id, (error, data) => {
+//     if (error) {
+//       res.json({
+//         message: "Internal Error",
+//         status: "false"
+//       })
+//     } else {
+//       res.json({
+//         message: "Data Deleted Successfully",
+//         status: "true"
+//       })
+//     }
+//   })
+// })
 
-//Edit Todo
-app.put("/todos/:id", (req, res) => {
-  const { id } = req.params
-  const { todo } = req.body;
-  if (!todo) {
-    res.json({
-      message: "Required Fields Are Missing",
-      status: "false"
-    })
-  } else {
-    const objToSend = {
-      todo: todo
-    }
-    todoModel.findByIdAndUpdate(id, objToSend, (error, data) => {
-      if (error) {
-        res.json({
-          message: "Internal Error",
-          status: "false"
-        })
-      } else {
-        res.json({
-          message: "Data Updated Successfully",
-          status: "true"
-        })
-      }
-    })
-  }
-})
-
-
-
-
-                            //Cart
-//Cart Collection
-app.get("/card", (req, res) => {
-  cardModel.find({}, (error, data) => {
-    if (error) {
-      res.json({
-        message: "Internal Error",
-        status: "false"
-      })
-    } else {
-      res.json({
-        message: "Data Get Successfully",
-        data: data,
-        status: "true"
-      })
-    }
-  })
-})
-//Add to Cart
-app.post("/card", (req, res) => {
-  const { img, name, text, color, size, rating, price , quantity } = req.body
-  if (!img || !name || !text || !color || !size || !rating || !price || !quantity){
-    res.json({
-      message: "Required fields are missing",
-      status: "false"
-    })
-  } else {
- cardModel.findOne({ name: name }, (error, data) => {
-      if (error) {
-        res.json({
-          message: "Internal Error",
-          status: "false"
-        })
-      } else if(data){
-        res.json({
-          quantity : quantity + 1,
-          message: "Add to cart successfully",
-          status: "true"
-        })
-      }else{
-        const objToSend = {
-          img: img,
-          name: name,
-          text: text,
-          color: color,
-          size: size,
-          rating: rating,
-          price: price,
-          quantity : quantity,
-        }
-        cardModel.create(objToSend, (error, data) => {
-          if (error) {
-            res.json({
-              message: "Internal Error",
-              status: "false"
-            })
-          } else {
-            res.json({
-              message: "Add to cart successfully",
-              data: data,
-              status: "true"
-            })
-          }
-        })
-      }
-  }) 
-  }
-})
-//Remove from Cart
-app.delete("/card/:id", (req, res) => {
-  const { id } = req.params
-  cardModel.findByIdAndDelete(id, (error, data) => {
-    if (error) {
-      res.json({
-        message: "Internal Error",
-        status: "false"
-      })
-    } else {
-      res.json({
-        message: "Removed from cart",
-        status: "true"
-      })
-    }
-  })
-})
+// //Edit Todo
+// app.put("/todos/:id", (req, res) => {
+//   const { id } = req.params
+//   const { todo } = req.body;
+//   if (!todo) {
+//     res.json({
+//       message: "Required Fields Are Missing",
+//       status: "false"
+//     })
+//   } else {
+//     const objToSend = {
+//       todo: todo
+//     }
+//     todoModel.findByIdAndUpdate(id, objToSend, (error, data) => {
+//       if (error) {
+//         res.json({
+//           message: "Internal Error",
+//           status: "false"
+//         })
+//       } else {
+//         res.json({
+//           message: "Data Updated Successfully",
+//           status: "true"
+//         })
+//       }
+//     })
+//   }
+// })
 
 
 
 
-                         //Wishlist
+//Cart
+// //Cart Collection
+// app.get("/card", (req, res) => {
+//   cardModel.find({}, (error, data) => {
+//     if (error) {
+//       res.json({
+//         message: "Internal Error",
+//         status: "false"
+//       })
+//     } else {
+//       res.json({
+//         message: "Data Get Successfully",
+//         data: data,
+//         status: "true"
+//       })
+//     }
+//   })
+// })
+// //Add to Cart
+// app.post("/card", (req, res) => {
+//   const { img, name, text, color, size, rating, price , quantity } = req.body
+//   if (!img || !name || !text || !color || !size || !rating || !price || !quantity){
+//     res.json({
+//       message: "Required fields are missing",
+//       status: "false"
+//     })
+//   } else {
+//  cardModel.findOne({ name: name }, (error, data) => {
+//       if (error) {
+//         res.json({
+//           message: "Internal Error",
+//           status: "false"
+//         })
+//       } else if(data){
+//         res.json({
+//           quantity : quantity + 1,
+//           message: "Add to cart successfully",
+//           status: "true"
+//         })
+//       }else{
+//         const objToSend = {
+//           img: img,
+//           name: name,
+//           text: text,
+//           color: color,
+//           size: size,
+//           rating: rating,
+//           price: price,
+//           quantity : quantity,
+//         }
+//         cardModel.create(objToSend, (error, data) => {
+//           if (error) {
+//             res.json({
+//               message: "Internal Error",
+//               status: "false"
+//             })
+//           } else {
+//             res.json({
+//               message: "Add to cart successfully",
+//               data: data,
+//               status: "true"
+//             })
+//           }
+//         })
+//       }
+//   }) 
+//   }
+// })
+// //Remove from Cart
+// app.delete("/card/:id", (req, res) => {
+//   const { id } = req.params
+//   cardModel.findByIdAndDelete(id, (error, data) => {
+//     if (error) {
+//       res.json({
+//         message: "Internal Error",
+//         status: "false"
+//       })
+//     } else {
+//       res.json({
+//         message: "Removed from cart",
+//         status: "true"
+//       })
+//     }
+//   })
+// })
+
+
+
+
+//Wishlist
 //WishList Collection
-app.get("/wishCard", (req, res) => {
-  wishCardModel.find({}, (error, data) => {
-    if (error) {
-      res.json({
-        message: "Internal Error",
-        status: "false"
-      })
-    } else {
-      res.json({
-        message: "Data Get Successfully",
-        data: data,
-        status: "true"
-      })
-    }
-  })
+// app.get("/wishCard", (req, res) => {
+//   wishCardModel.find({}, (error, data) => {
+//     if (error) {
+//       res.json({
+//         message: "Internal Error",
+//         status: "false"
+//       })
+//     } else {
+//       res.json({
+//         message: "Data Get Successfully",
+//         data: data,
+//         status: "true"
+//       })
+//     }
+//   })
+// })
+// //Add to Wishlist
+// app.post("/wishCard", (req, res) => {
+//   const { img, name, text, color, size, rating, price , quantity } = req.body
+//   if (!img || !name || !text || !color || !size || !rating || !price || !quantity) {
+//     res.json({
+//       message: "Required fields are missing",
+//       status: "false"
+//     })
+//   }else {
+//     wishCardModel.findOne({ name: name }, (error, data) => {
+//       if (error) {
+//         res.json({
+//           message: "Internal Error",
+//           status: "false"
+//         })
+//       } else if(data){
+//         res.json({
+//           message: "Already added to wishlist",
+//           status: "false"
+//         })
+//       }else{
+//         const objToSend = {
+//           img: img,
+//           name: name,
+//           text: text,
+//           color: color,
+//           size: size,
+//           rating: rating,
+//           price: price
+//         }
+//         wishCardModel.create(objToSend, (error, data) => {
+//           if (error) {
+//             res.json({
+//               message: "Internal Error",
+//               status: "false"
+//             })
+//           } else {
+//             res.json({
+//               message: "Add to wishlist successfully",
+//               data: data,
+//               status: "true"
+//             })
+//           }
+//         })
+
+//       }
+//     })
+
+
+
+
+
+
+
+
+
+
+//   }
+// })
+// //Remove from wishlist
+// app.delete("/wishCard/:id", (req, res) => {
+//   const { id } = req.params
+//   wishCardModel.findByIdAndDelete(id, (error, data) => {
+//     if (error) {
+//       res.json({
+//         message: "Internal Error",
+//         status: "false"
+//       })
+//     } else {
+//       res.json({
+//         message: "Removed from wishlist",
+//         status: "true"
+//       })
+//     }
+//   })
+// })
+
+
+
+// //Requester
+
+// const mongoose = require("mongoose")
+
+const requesterSchema = mongoose.Schema({
+  curr_latitude: Number,
+  curr_longitude: Number,
+  category: String,
+  sub_category: String,
+  name: String,
+
 })
-//Add to Wishlist
-app.post("/wishCard", (req, res) => {
-  const { img, name, text, color, size, rating, price , quantity } = req.body
-  if (!img || !name || !text || !color || !size || !rating || !price || !quantity) {
-    res.json({
-      message: "Required fields are missing",
-      status: "false"
-    })
-  }else {
-    wishCardModel.findOne({ name: name }, (error, data) => {
-      if (error) {
-        res.json({
-          message: "Internal Error",
-          status: "false"
-        })
-      } else if(data){
-        res.json({
-          message: "Already added to wishlist",
-          status: "false"
-        })
-      }else{
-        const objToSend = {
-          img: img,
-          name: name,
-          text: text,
-          color: color,
-          size: size,
-          rating: rating,
-          price: price
-        }
-        wishCardModel.create(objToSend, (error, data) => {
-          if (error) {
-            res.json({
-              message: "Internal Error",
-              status: "false"
-            })
-          } else {
-            res.json({
-              message: "Add to wishlist successfully",
-              data: data,
-              status: "true"
-            })
-          }
-        })
-
-      }
-    })
-
-    
-   
+const requesterModel = mongoose.model("Requester", requesterSchema)
+module.exports = requesterModel
 
 
-
-
-
-
-
-  }
-})
-//Remove from wishlist
-app.delete("/wishCard/:id", (req, res) => {
-  const { id } = req.params
-  wishCardModel.findByIdAndDelete(id, (error, data) => {
-    if (error) {
-      res.json({
-        message: "Internal Error",
-        status: "false"
-      })
-    } else {
-      res.json({
-        message: "Removed from wishlist",
-        status: "true"
-      })
-    }
-  })
-})
-    
-
-
-//Requester
 app.get("/requester", (req, res) => {
   requesterModel.find({}, (error, data) => {
     if (error) {
@@ -516,38 +531,38 @@ app.get("/requester", (req, res) => {
 
 
 app.post("/requester", (req, res) => {
-  const {currLatitude , currLongitude , category , subCategory , name } = req.body
+  const { currLatitude, currLongitude, category, subCategory, name } = req.body
   if (!currLatitude || !currLongitude || !category || !subCategory || !name) {
     res.json({
       message: "Required fields are missing",
       status: "false"
     })
-  }else {
-        const objToSend = {
-          curr_latitude: currLatitude,
-          curr_longitude: currLongitude,
-          category: category,
-          sub_category: subCategory,
-          name: name
-        }
-        requesterModel.create(objToSend, (error, data) => {
-          if (error) {
-            res.json({
-              message: "Internal Error",
-              status: "false"
-            })
-          } else {
-            res.json({
-              message: "Data sent successfully",
-              data: data,
-              status: "true"
-            })
-          }
+  } else {
+    const objToSend = {
+      curr_latitude: currLatitude,
+      curr_longitude: currLongitude,
+      category: category,
+      sub_category: subCategory,
+      name: name
+    }
+    requesterModel.create(objToSend, (error, data) => {
+      if (error) {
+        res.json({
+          message: "Internal Error",
+          status: "false"
         })
+      } else {
+        res.json({
+          message: "Data sent successfully",
+          data: data,
+          status: "true"
+        })
+      }
+    })
   }
 })
 
-                  
+
 
 
 
